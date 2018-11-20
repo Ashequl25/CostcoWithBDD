6c85cd3b6b7de8d10909d3635517dceb1c958652
Ashequl25 <ashequl.sust@gmail.com>
2018-11-19 19:40:52 -0500

    stepFiles added for Sign In process

--- src/test/java/allFeaturesFiles/SignIn.feature
+++ src/test/java/allFeaturesFiles/SignIn.feature
@@ -1,1 +1,9 @@
 Feature: Sign In to Costco
+  This feature deals with sign in or registration process in Costco
+
+  Scenario: Sign In in Costco
+    Given User is in HomePage
+    When User clicks on Signn In/Register button on HomePage
+    Then User validates the Sign In page
+    And User provides the email and password
+    Then User clicks on Sign In Button to register
--- src/test/java/allStepDefinitions/StepDefinitions.java
+++ src/test/java/allStepDefinitions/StepDefinitions.java
@@ -1,5 +1,33 @@
 package allStepDefinitions;
 
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 public class StepDefinitions {
+	@Given("^User is in Costco HomePage$")
+	public void user_is_in_Costco_HomePage() throws Throwable {
+	 
+	}
+
+	@When("^User clicks on Sign In/Rgister Button$")
+	public void user_clicks_on_Sign_In_Rgister_Button() throws Throwable {
+	  
+	}
+
+	@Then("^User validates the Sign In page$")
+	public void user_validates_the_Sign_In_page() throws Throwable {
+	   
+	}
+
+	@When("^User provides email id and password$")
+	public void user_provides_email_id_and_password() throws Throwable {
+	   
+	}
+
+	@When("^User click on Sign In button$")
+	public void user_click_on_Sign_In_button() throws Throwable {
+	   
+	}
 
 }
